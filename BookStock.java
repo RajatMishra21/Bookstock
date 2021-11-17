f598c7e90cf22ea55262e913fbea6e5bf1c37ec5
Rajat Mishra <94281732+RajatMishra21@users.noreply.github.com>
2021-11-17 20:02:11 +0530

    Add files via upload

---
+++ BookStock.java
@@ -0,0 +1,42 @@
+class Book
+{
+    private int qtyInStock;
+    private final String name,author;
+    private  double price;
+
+    Book(int qtyInStock, String name, String author, double price)
+    {
+        this.qtyInStock = qtyInStock;
+        this.name=name;
+        this.author=author;
+        this.price=price;
+    }
+    public int getQtyInStock()
+    {
+        return qtyInStock;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getAuthor()
+    {
+        return author;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+
+}
+
+public class BookStock {
+    public static void main(String[] args) {
+        Book b1 = new Book(340,"Java is Easy","HK Verma",550.50);
+        System.out.println("Book's Name: " + b1.getName());
+        System.out.println("Author's Name: " + b1.getAuthor());
+        System.out.println("Stock: " + b1.getQtyInStock());
+        System.out.println("Price: " + b1.getPrice());
+    }
+}
+
